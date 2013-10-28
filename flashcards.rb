ed920acfd6e643272d2a4e3b38c3d8aa7d9f23fd
Zezan Tam <tam.zezan@gmail.com>
2013-10-28 22:04:42 +1100

    first intro line added

--- flashcards.rb
+++ flashcards.rb
@@ -1,1 +1,6 @@
-# Main file for 
+# Main file for Flashcards game
+
+#Welcome to the game
+puts "Welcome to Flashcards, built to test knowledge of Ruby. To play, simply enter the correct term for each definition. Ready? Go!"
+
+#
